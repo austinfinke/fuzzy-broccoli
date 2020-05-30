ddb464367b0b947ce4eadb93fd88829955c5fccf
Austin Finke <austinfinke28@gmail.com>
2020-05-30 00:59:52 -0700

    hovernav

--- js/main.js
+++ js/main.js
@@ -13,7 +13,11 @@ function docReady(fn) {
 
 docReady(() => {
   coursehover();
+  abouthover();
+  homehover();
 });
+
+//TODO: optimize single function
 
 const coursehover = () => {
   const courseLI = document.querySelector(".js--main-nav-courses-li");
@@ -24,6 +28,48 @@ const coursehover = () => {
   });
 
   courseLI.addEventListener("mouseout", (e) => {
+    hoverMenu.classList.remove("show");
+  });
+
+  hoverMenu.addEventListener("mouseover", (e) => {
+    hoverMenu.classList.add("show");
+  });
+
+  hoverMenu.addEventListener("mouseout", (e) => {
+    hoverMenu.classList.remove("show");
+  });
+};
+
+const abouthover = () => {
+  const aboutLI = document.querySelector(".js--main-nav-about-li");
+  const hoverMenu = document.querySelector(".menu-about");
+
+  aboutLI.addEventListener("mouseover", (e) => {
+    hoverMenu.classList.add("show");
+  });
+
+  aboutLI.addEventListener("mouseout", (e) => {
+    hoverMenu.classList.remove("show");
+  });
+
+  hoverMenu.addEventListener("mouseover", (e) => {
+    hoverMenu.classList.add("show");
+  });
+
+  hoverMenu.addEventListener("mouseout", (e) => {
+    hoverMenu.classList.remove("show");
+  });
+};
+
+const homehover = () => {
+  const homeLI = document.querySelector(".js--main-nav-home-li");
+  const hoverMenu = document.querySelector(".menu-home");
+
+  homeLI.addEventListener("mouseover", (e) => {
+    hoverMenu.classList.add("show");
+  });
+
+  homeLI.addEventListener("mouseout", (e) => {
     hoverMenu.classList.remove("show");
   });
 

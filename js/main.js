3f9ddd2c1f468d0b5a3127fad63f15b38982a1f1
Austin Finke <austinfinke28@gmail.com>
2020-05-30 23:58:20 -0700

    mobile

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(() => {
   itemClick("home");
   itemClick("about");
   onScroll();
+  animateArrow();
 });
 
 const itemHover = (item) => {
@@ -51,6 +52,23 @@ function onScroll() {
   document.addEventListener("scroll", () => {
     arrow.style.display = "none";
   });
+}
+
+function animateArrow() {
+  const arrow = $(".fa-arrow-circle-down");
+  arrow.animate({ top: "90%" }, 30);
+  arrow.animate({ top: "91%" }, 30);
+  arrow.animate({ top: "92%" }, 30);
+  arrow.animate({ top: "93%" }, 30);
+  arrow.animate({ top: "94%" }, 30);
+  arrow.animate({ top: "95%" }, 30);
+  arrow.animate({ top: "96%" }, 30);
+  arrow.animate({ top: "95%" }, 50);
+  arrow.animate({ top: "94%" }, 50);
+  arrow.animate({ top: "93%" }, 60);
+  arrow.animate({ top: "94%" }, 50);
+  arrow.animate({ top: "95%" }, 40);
+  arrow.animate({ top: "96%" }, 30);
 }
 
 /**
